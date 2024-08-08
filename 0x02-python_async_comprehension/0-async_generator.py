f77f62adec8ad815e3ff57a94f78b85a67a48b2a
root <root@DESKTOP-99J7PEJ>
2024-08-08 20:39:17 +0100

    py

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Type-annotated asynchronous coroutine """
+
+from random import uniform
+from asyncio import sleep
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """ Async function that waits 1 second then yields random num 10 times """
+    for x in range(10):
+        # Asyncio sleep is non-blocking while time sleep is blocking
+        await sleep(1)
+        yield uniform(1, 10)
