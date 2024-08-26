33d9a3cb1c4e07a57197c3735c65802af6da7a66
root <root@DESKTOP-99J7PEJ>
2024-08-26 23:12:19 +0100

    uni

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+""" Unit tests for client.py """
+from client import GithubOrgClient
+from parameterized import parameterized, parameterized_class
+from unittest import TestCase, mock
+from unittest.mock import patch, MagicMock, PropertyMock
+from urllib.error import HTTPError
+from fixtures import *
+
+
+class TestGithubOrgClient(TestCase):
+    """ Tests for GithubOrgClient methods """
+
+    @parameterized.expand([
+        ("google"),
+        ("abc")
+    ])
+    # MagicMock creates attributes and methods
+    # org returns jsonized dict - so specify return_value dict
+    @patch("client.get_json",
+           MagicMock(return_value={'key': 'value'}))
+    def test_org(self, org_name):
+        """ Test for GithubOrgClient.org method """
+        cls = GithubOrgClient(org_name)
+        self.assertEqual(cls.org, {'key': 'value'})
+
+    def test_public_repos_url(self):
+        """ Test for GithubOrgClient._public_repos_url method """
+        # PropertyMock provides getter/setter methods and calls with no args
+        # _public_repos_url returns value for key repos_url
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock,
+                   return_value={"repos_url": "url"}):
+            # Method doesn't take args but need to mock with org_name for org
+            cls = GithubOrgClient('org_name')
+            self.assertEqual(cls._public_repos_url, 'url')
+
+    @patch('client.get_json')
+    def test_public_repos(self, license):
+        """ Test for GithubOrgClient.public_repos method """
+        # PropertyMock provides getter/setter methods and calls with no args
+        # public_repos returns value from license using _public_repos_url
+        with patch('client.GithubOrgClient.public_repos',
+                   new_callable=PropertyMock) as mock_repo:
+            cls = GithubOrgClient('org_name')
+            # Mock license dict
+            license.return_value = {'repos_url': 'url'}
+            # Pass org_name to org to retrieve license dict
+            mock_repo.return_value = cls.org.get('repos_url')
+            self.assertEqual(cls.public_repos, 'url')
+            license.assert_called_once()
+            mock_repo.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """ Test for GithubOrgClient.has_license if repo has license """
+        cls = GithubOrgClient('org_name')
+        self.assertEqual(cls.has_license(repo, license_key), expected)
+
+
+@parameterized_class(
+    ("org_payload", "repos_payload", "expected_repos", "apache2_repos"),
+    TEST_PAYLOAD
+)
+class TestIntegrationGithubOrgClient(TestCase):
+    """ Integration tests for GithubOrgClient.public_repos method """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up class for integration tests
+        Mock requests.get to return payload
+        """
+        cls.get_patcher = patch("requests.get", side_effect=HTTPError)
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Teardown for integration tests
+        Stop mocking request.get
+        """
+        cls.get_patcher.stop()
---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+""" Unit tests for utils.py """
+from parameterized import parameterized
+from unittest import TestCase, mock
+from utils import access_nested_map, get_json, memoize
+
+
+class TestAccessNestedMap(TestCase):
+    """ Tests for access_nested_map function """
+
+    # tuple of (nested_map, path, expected value)
+    @parameterized.expand([
+        ({"a": 1}, ("a"), 1),
+        ({"a": {"b": 2}}, ("a"), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """ Test for access_nested_map function """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    # tuple of (nested_map, path) - expect KeyError
+    @parameterized.expand([
+        ({}, ("a")),
+        ({"a": 1}, ("a", "b")),
+        ({"a": {"b": 2}}, ("b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """ Test for key error failures of access_nested_map function """
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
+
+
+class TestGetJson(TestCase):
+    """ Tests for get_json function """
+    # tuple of (url, payload dict)
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url, payload):
+        """ Test for get_json returning payload """
+        with mock.patch("requests.get") as req:
+            # Set return value of requests.get to payload dict
+            req().json.return_value = payload
+            # Verify that when pass in url, recieve payload dict
+            self.assertEqual(get_json(url), payload)
+
+
+class TestMemoize(TestCase):
+    """ Tests for memoize function """
+
+    def test_memoize(self):
+        """ Test for asserting that memoize function sets attr """
+        class TestClass:
+            """ Test class for memoize function """
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        # MagicMock to mock method/return value
+        test1 = TestClass()
+        test1.a_method = mock.MagicMock(return_value=42)
+        self.assertEqual(test1.a_property, 42)
+        self.assertEqual(test1.a_property, 42)
+        test1.a_method.assert_called_once()
+
+        # patch.object to mock method/return value
+        with mock.patch.object(TestClass, "a_method",
+                               return_value=42) as mock_method:
+            test2 = TestClass()
+            self.assertEqual(test2.a_property, 42)
+            self.assertEqual(test2.a_property, 42)
+            mock_method.assert_called_once()
